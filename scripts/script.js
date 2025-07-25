3848b9123cbfae28b5d6b877e955fddb7e5edf29
K1111n <kaja_nan@hotmail.de>
2025-07-25 13:38:37 +0200

    feat: async function fetchPokemon();

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
-searchForPokemonWhenThreeCharactersEntered() {
-    if (input.value.length < 3) {
-        return;
-    } else {
-        searchForPokemon();
-    }
+function searchForPokemonWhenThreeCharactersEntered() {
+  if (input.value.length < 3) {
+    return;
+  } else {
+    searchForPokemon();
+  }
 }
 
-searchForPokemon() {}
+function searchForPokemon() {}
+
+async function fetchPokemon() {
+  let response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+  );
+  let responseAsJson = await response.json();
+  console.log(responseAsJson);
+}
